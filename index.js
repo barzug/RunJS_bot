c802dacb62d146823cb4933ff114ffa5b024c594
Sergey Barsukov <barsukof@live.com>
2018-01-29 14:33:24 +0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-// let console = {
-//     _message: ""
-// }
+const Telegraf = require('telegraf')
 
-// console.prototype.log = function () {
-
-// }
-
-try {
-    eval();
-} catch (e) {
-    console.log(e);
+const ConsoleMock = function() {
+    this._message = '';
 }
+
+ConsoleMock.prototype.log = function (text) {
+    this._message += `\n${text}`
+};
+
+ConsoleMock.prototype.getMessage = function () {
+    return this._message;
+};
+
+
+const bot = new Telegraf('534174738:AAG4OcO3uRif_TzSUt9o6WbtA9jwDTJ5r04');
+
+bot.start((ctx) => ctx.reply('Hey!'))
+
+bot.on('message', (ctx) => {
+    let console = new ConsoleMock();
+
+    try {
+        eval(ctx.message.text);
+    } catch (e) {
+        console.log(e);
+    }
+
+    ctx.reply(console.getMessage());
+});
+
+bot.startPolling();
